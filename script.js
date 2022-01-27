b755e2f199170d1adbc88286b55fe47b26939e9d
Hadijat <93347168+Hadijat@users.noreply.github.com>
2022-01-27 20:59:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+ 
+ let num = 0;
+
+ while(num < 1) {
+  const a = prompt("Один из последних просмотренных фильмов?"),
+  b = prompt("На сколько оцените его?");
+  num++;
+
+
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+  personalMovieDB[a] = b;
+   console.log('Error');
+  num--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+  } else {
+  console.log("Произошла ошибка");
+}
+
+
+
